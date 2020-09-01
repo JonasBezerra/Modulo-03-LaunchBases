2b8c2539ec09ecc3d8333d1a4374315614286eb7
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-09-01 16:02:10 -0400

    Inicializando servidor e add templates engines

--- server.js
+++ server.js
@@ -17,7 +17,29 @@ server.listen(5000, function () {
 qnd ele deu a mensagem Cannot get / no navegador ele disse
 que não esta achando a rota barra que é a rota para o meu arquivo principal*/ 
 server.get("/", function (req, res) {
-    return res.render("views/index.html")
+    return res.render("index")
+    /*
+        SEGUNDO PARAMETRO é um método que tem dois parâmetros.O primeiro para requisição 
+        ou envio de alguma coisa. O segundo é para dá uma resposta para o usuario.
+    */
+})
+
+/*Configurando minha template Engenie */
+// declarar o tipo de motor de view que eu vou usar aqui
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {express: server }) 
+/*(digo o caminho, tipo. no caso em formato de objetos) */
+
+/*  =========== configurando servidor para receber CSS ======*/
+
+server.use(express.static('public'))
+
+/* Agora vamos criar a rota para o arquivo portifolio.html */
+
+server.get("/portifolio", function (req, res) {
+    return res.render("portifolio")
     /*
         SEGUNDO PARAMETRO é um método que tem dois parâmetros.O primeiro para requisição 
         ou envio de alguma coisa. O segundo é para dá uma resposta para o usuario.
